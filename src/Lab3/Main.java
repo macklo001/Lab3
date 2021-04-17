3083481d72df34d8d4c3fcd84edbdf847bb55e25
barbab002 <barbab002@utp.edu.pl>
2021-04-17 09:49:36 +0200

    Lab3_1

--- src/Lab3/Main.java
+++ src/Lab3/Main.java
@@ -3,8 +3,8 @@ package Lab3;
 public class Main {
 
     public static void main(String[] args) {
-        Task zad1 = new Task("Sprzatanie", "Posprzatac pokoj", TaskCategory.IN_PROGRESS);
-        Subtask zad2 = (Subtask) new Task("Trening", "Pojsc na silownie", TaskCategory.DONE);
+        Task zad1 = new Task("Milość", "Sen", TaskCategory.IN_PROGRESS);
+        Subtask zad2 = (Subtask) new Task("Liscie", "opadają", TaskCategory.DONE);
         Task zad3 = (Task) new Object();
         Subtask zad4 = (Subtask) new Object();
     }
